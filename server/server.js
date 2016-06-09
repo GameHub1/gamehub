88feab5b106ce3e0ff96fd968a3af753f0d8c6d1
Sam Richards <samrrichards@gmail.com>
2016-06-09 11:54:40 -0700

    [fix] refactored server to use es6 syntax

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../dist/')));
 
-app.listen(process.env.PORT || 3333);
+app.listen(process.env.PORT || 3000);
 
-console.log("Now listening on Port 3000"); 
+console.log("Now listening on Port 3000");
