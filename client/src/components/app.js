d2172818dffac3d704574243cdc30bec543e9170
Gina Zhou <ginayizhou@gmail.com>
2016-06-11 10:47:43 -0700

    [feat] initialize auth0 login

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,21 +2,19 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import FbLogin from '../containers/fb_login';
 // import Login from '../containers/login';
+import Auth0 from './auth0_login';
 
 export default class App extends Component {
+
   render() {    
     return (
       <div>
         <h1>GameHub</h1>
         <h2>slogan...</h2>
         <FbLogin />
-        Test, 1,2
-      {this.props.children}
-      </div>
-
-      
-      
-      
+        {this.props.children}
+        <Auth0 />
+      </div> 
     );
   }
 }
