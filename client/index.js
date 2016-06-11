c88659a141f2245407c76075a62a56ce784c327b
metamonkey8 <chen.liu.michael@gmail.com>
2016-06-10 19:00:38 -0700

    [feat] CSS bundle

--- client/index.js
+++ client/index.js
@@ -6,6 +6,8 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './src/components/app';
 // import LoginReducer from './src/reducers/login_reducer';
 
+require(__dirname + "/css/style.css");
+
 ReactDOM.render(
   <App />,
   document.getElementById('app'));
