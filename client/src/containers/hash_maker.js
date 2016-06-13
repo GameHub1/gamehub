b3fe2f60531ab61bcebc629c646d466935bc2752
Sam Richards <samrrichards@gmail.com>
2016-06-13 10:59:19 -0700

    [fix] removed unncessary hashing file

--- client/src/containers/hash_maker.js
+++
@@ -1,8 +0,0 @@
-import Hashes from 'jshashes';
-
-export default function(string) {
-  let hashedStr = "";
-  hashedStr += new Hashes.SHA1().b64(string);
-  hashedStr += new Hashes.MD5().b64(string);
-  return hashedStr; 
-}
