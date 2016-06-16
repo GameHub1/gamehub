37620d732c5594c32b7d5087aad1da63638d646b
Gina Zhou <ginayizhou@gmail.com>
2016-06-16 16:36:30 -0700

    [feat] create nav bar with search bar and create homepage

--- client/src/components/root_component.js
+++ client/src/components/root_component.js
@@ -1,19 +1,34 @@
 import React, {Component} from 'react';
-
+import {connect} from 'react-redux';
+import ReduxLogin from './login_redux';
+import SearchBar from '../containers/search_bar';
+import SearchedUsersList from '../containers/searched_users_list';
 
 export default class RootComponent extends Component {
-  
-   render () {
+  render () {
+    console.log('root', Array.isArray(this.props.authData));
+    if (!Array.isArray(this.props.authData)) {
       return (
-          <div>
-          <header>
-            <h1> GAMEHUB </h1>
-          </header>
+        <div>
+          <SearchBar /><br/>
+          <SearchedUsersList /> 
           {this.props.children}
-          </div>
-
-        )
-   }
-
-
+        </div>
+      );
+    } else {
+      return (
+        <div id="mario" background={require(__dirname + "/../../images/mario.jpg")}>
+          <h1 id="headerTitle">GameHub</h1>
+          <h2 id="headerSub">Come join the dark side...</h2>
+          <ReduxLogin />
+        </div>
+      );
+    }
+  }
 }
+
+function mapStateToProps({authData}) {
+  return {authData};
+}
+
+export default connect(mapStateToProps)(RootComponent);
