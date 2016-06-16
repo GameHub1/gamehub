159506797cb25ac516acb7ee338c8f63c9b54fcb
Gina Zhou <ginayizhou@gmail.com>
2016-06-16 10:06:50 -0700

    [feat] render searched users in a search list

--- client/src/reducers/searched_users_reducer.js
+++ client/src/reducers/searched_users_reducer.js
@@ -3,8 +3,7 @@ import {FETCH_USERS} from '../actions/index';
 export default function (state=[], action) {
   switch(action.type) {
     case FETCH_USERS:
-      let searched_users = action.payload;
-      return searched_users;
+      return action.payload.data;
     default: 
       return state;
   }
