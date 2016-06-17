134dad5fe5bcd881725463fcc70bf44e8bc0e48b
Kyle <kyle.mcgourty@gmail.com>
2016-06-16 17:36:13 -0700

    [fix] changed format of profile state data in form

--- client/src/reducers/profile_reducer.js
+++ client/src/reducers/profile_reducer.js
@@ -3,6 +3,7 @@ import {POST_PROFILE} from '../actions/index.js';
 export default function (state=[], action) {
   switch(action.type) {
     case POST_PROFILE:
+      console.log('inside profile reducer');
       return action.payload;
     default: 
       return state;
