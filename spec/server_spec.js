f57698cca4adfb62029d4a30202ca741bf0e265f
Gina Zhou <ginayizhou@gmail.com>
2016-06-23 09:31:25 -0700

    [feat] switch server-side testing to Jasmine and supertest

--- spec/server_spec.js
+++ spec/server_spec.js
@@ -1,15 +1,39 @@
-var request = require('request');
-
-var base_url = 'http://localhost:8000/';
+var request = require('supertest-as-promised');
+var app = require('../server/server');
 
 describe ('Server Loading', function() {
   describe ('GET /', function() {
     it ('returns status code 200', function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
+      request(app)
+        .get('/')
+        .then(function(res) {
+          expect(res.statusCode).toEqual(200);
+          done();
+        })
+        .catch(done.fail);
     });
+  });
+});
 
+describe ('Sign Up', function() {
+  beforeEach(function() {
+    this.mockUser = {
+      name: 'Tester1', 
+      email: 'hello@mks.com', 
+      pic_path: 'testimage.jpg', 
+      routeProp: 'val'
+    };
+  });
+
+  describe ('POST /signup', function() {
+    it ('console logs "User is already in database."', function(done) {
+      request(app)
+        .post('/signup')
+        .send(this.mockUser)
+        .expect(200)
+        .then(function(res) {
+          done();
+        });
+    });
   });
 });
