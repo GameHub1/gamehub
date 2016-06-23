5e8980e85fa86d49fbb6e57e94ceacaf576d39d7
Sam Richards <samrrichards@gmail.com>
2016-06-23 15:15:33 -0700

    [refactor] paused work on delete game feature

--- client/src/reducers/games_reducer.js
+++ client/src/reducers/games_reducer.js
@@ -1,12 +1,17 @@
 import {CREATE_GAME} from '../actions/index.js';
+import {DELETE_GAME} from '../actions/index.js';
 import {FETCH_GAMES} from '../actions/index.js';
 
 export default function (state=[], action) {
   switch(action.type) {
     case CREATE_GAME:
     console.log("Game reducer consoled")
-     let game = [action.payload[0].gameTitle]
+     let game = [action.payload[0].gameTitle];
       return [state, ...game];
+    case DELETE_GAME:
+      let deletedGame = [action.payload[0].gameTitle];
+      console.log("Deleting game: ", deletedGame);
+      return state;
     case FETCH_GAMES:
       console.log("Fetching games: ", action.payload.data.data);
       return action.payload.data.data;
