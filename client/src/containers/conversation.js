57061e42f4bbaab9969e9e8306328ba1ed02d7e3
Sam Richards <samrrichards@gmail.com>
2016-06-25 10:49:46 -0700

    [fix] added files that get deleted on merge

---
+++ client/src/containers/conversation.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+export class Conversation extends Component {
+  render(){
+    return (
+      <section>
+        <div className="conversation">
+          Conversation here:
+        </div>
+      </section>
+    );
+  }
+}
+
+export default connect(null, null)(Conversation); 
