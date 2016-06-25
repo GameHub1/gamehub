d53be9cd96a2068c6fcd642b4d3019c0f05d537c
Sam Richards <samrrichards@gmail.com>
2016-06-25 14:20:41 -0700

    [feat] created message-related routes

---
+++ server/message_utils.js
@@ -0,0 +1,11 @@
+exports.sendMessage = function(req, res){
+  console.log("sendMessage function called!");
+};
+
+exports.loadNamespace = function(req, res){
+  console.log("loadNamespace function called!");
+};
+
+exports.createNamespace = function(){
+  console.log("createNamespace function called!");
+}; 
