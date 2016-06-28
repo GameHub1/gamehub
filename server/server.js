b3c39e0f0ae3e5de06245ffa2b8b7a1b9a74e82a
Sam Richards <samrrichards@gmail.com>
2016-06-27 19:55:03 -0700

    [refactor] created new file for socket.io route

--- server/server.js
+++ server/server.js
@@ -5,92 +5,52 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const auth = require('./utils/auth_utils.js')
-const messaging = require('./utils/message_utils.js');
-const favmedia = require('./utils/favmedia_utils.js');
-const games = require('./utils/game_utils.js');
-const social = require('./utils/social_utils.js');
-const user = require('./utils/user_utils.js');
+const authUtils = require('./utils/auth_utils.js')
+const messagingUtils = require('./utils/message_utils.js');
+const favmediaUtils = require('./utils/favmedia_utils.js');
+const gamesUtils = require('./utils/game_utils.js');
+const socialUtils = require('./utils/social_utils.js');
+const userUtils = require('./utils/user_utils.js');
+const socketUtils = require('./utils/socket_utils.js')
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 //server.listen(80);
 
-// io.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 app.use(bodyParser.json({type: '*/*'}));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, '../dist/')));
 
-app.post('/signup', auth.authFunc);
+app.post('/signup', authUtils.authFunc);
 
-app.post('/get_user_info', user.userInfo);
-app.post('/post_profile', user.editProfile);
+app.post('/get_user_info', userUtils.userInfo);
+app.post('/post_profile', userUtils.editProfile);
 
-app.post('/favmedia', favmedia.getFavmedia);
+app.post('/favmedia', favmediaUtils.getFavmedia);
 
-app.post('/games', games.newGame);
-app.post('/fetch_games', games.fetchGames);
+app.post('/games', gamesUtils.newGame);
+app.post('/fetch_games', gamesUtils.fetchGames);
+app.post('/delete_game', gamesUtils.deleteGame);
+app.post('/show_game_fans', gamesUtils.showGameFans);
 
-app.post('/get_users', social.fetchUsers);
-app.post("/show_friends", social.showFriends);
-app.post('/get_friend_info', social.friendInfo);
-app.post('/add_friend', social.addFriend);
+app.post('/get_users', socialUtils.fetchUsers);
+app.post("/show_friends", socialUtils.showFriends);
+app.post('/get_friend_info', socialUtils.friendInfo);
+app.post('/add_friend', socialUtils.addFriend);
 
-app.post('/send_message', messaging.sendMessage);
-app.post('/fetch_messages', messaging.fetchMessages);
-app.post('/create_namespace', messaging.createNamespace);
+app.post('/send_message', messagingUtils.sendMessage);
+app.post('/fetch_messages', messagingUtils.fetchMessages);
+app.post('/create_namespace', messagingUtils.createNamespace);
 
-app.post('/signup', auth.authFunc);
-
-app.post('/delete_game', function(req, res){
-  let gameTitle = req.body[0].gameTitle;
-  let email = req.body[1];
-  let joinReq = {users_id_fk: 0, games_id_fk: 0};
-  console.log(gameTitle, email);
-
-  setTimeout(function(){
-    new Game({name: gameTitle}).fetch().then(model => {
-      joinReq.games_id_fk = model.get('id');
-      console.log("gameID: ", joinReq.games_id_fk);
-      console.log("setTimeout join req:", joinReq);
-      deleteGameJoin(joinReq);
-    })
-    .catch(err => {
-      console.error(err);
-    });
-  }, 500);
-
-  new User({email: email}).fetch().then(model => {
-    joinReq.users_id_fk  = model.get('id');
-    console.log("User ID: ", joinReq.users_id_fk);
-  })
-  .catch(err => {
-    console.error(err);
-  });
-});
-
-app.post("/show_game_fans", function(req,res) {
-  let game = req.body.game;
-  bookshelf.knex.raw("SELECT * FROM users WHERE users.id IN (SELECT users_games.users_id_fk FROM games inner JOIN users_games ON users_games.games_id_fk = games.id WHERE games.name = '" + game + "');")
-    .then(response => {
-      let info = response.rows.reduce((acc, cur) => {
-        acc.push({name: cur.fullname, email: cur.email, pic_path: cur.pic_path});
-        return acc;
-      }, []);
-      console.log(info);
-      res.send({data: info});
-    })
-    .catch(err => {
-      console.error(err);
-    });
-});
+app.post('/get_messages', socketUtils.loadSocket);
 
 app.use(function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
---
+++ server/utils/socket_utils.js
@@ -0,0 +1,20 @@
+exports.loadSocket = function(req, res) {
+  let namespace = req.body.data
+  console.log(namespace);
+  let channel = io.of(`/${namespace}`);
+
+   channel.on('connection', function (socket) {
+      console.log('We are connected!');
+       socket.on('message', function (msg) {
+         //store msg to database
+         socket.emit('updateState', 'update')
+      });
+   });
+   let kylemike = io.of('/kyle');
+    kylemike.on('connection', function (socket) {
+      console.log("Houston, we have connected");
+      socket.on('message', function (msg) {
+      socket.emit('message', "Original msg:" + msg + "This is from the server");
+    });
+  });
+}; 
