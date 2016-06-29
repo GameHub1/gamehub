5db12db1c66d7c301c173f01efad581a266b926e
Sam Richards <samrrichards@gmail.com>
2016-06-29 10:49:55 -0700

    [fix] tentative solution to db connection error

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const socketUtils = require('./utils/socket_utils.js');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-//server.listen(8080);
+server.listen(8080);
 
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
