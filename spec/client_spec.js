e3024957c5c7a6e9eb3d6f5e208d88940a8514ae
Gina Zhou <ginayizhou@gmail.com>
2016-06-29 11:44:14 -0700

    [test] trying out testing with enzyme

--- spec/client_spec.js
+++ spec/client_spec.js
@@ -1,18 +1,19 @@
-const React = require('react');
-const jasmineEnzyme = require('jasmine-enzyme');
-const shallow = require('enzyme').shallow;
-// const SearchBar = require('../client/src/containers/search_bar');
+require('babel-register')();
+// const React = require('react');
+// const jasmineEnzyme = require('jasmine-enzyme');
+// const shallow = require('enzyme').shallow;
+// // const SearchBar = require('../client/src/containers/search_bar');
 
-describe('<SearchBar />', () => {
-  beforeEach(() => {
-    jasmineEnzyme();
-  });
+// describe('<SearchBar />', () => {
+//   beforeEach(() => {
+//     jasmineEnzyme();
+//   });
 
-  // it('has a form', () => {
-  //   const wrapper = shallow(<SearchBar />);
-  //   expect(wrapper.find('form')).to.have.length(1);
-  // });
-});
+//   // it('has a form', () => {
+//   //   const wrapper = shallow(<SearchBar />);
+//   //   expect(wrapper.find('form')).to.have.length(1);
+//   // });
+// });
 
 
 
